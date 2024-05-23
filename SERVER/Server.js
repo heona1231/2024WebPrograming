8484fdaefda1df8f35472d67817f88ea35e71519
heona1231 <heona1231@gmail.com>
2024-05-23 12:00:00 +0900

    update 0523

--- SERVER/Server.js
+++ SERVER/Server.js
@@ -1,12 +1,29 @@
-var mysql = require("mysql");
-var conn = mysql.createConnection({
-    host:"localhost",
-    user:"root",
-    password:"1231",
-    database:"yourboard",
-});
+const username = encodeURIComponent("user");
+const password = encodeURIComponent("1231");
 
-conn.connect();
+const mongoclient = require('mongodb').MongoClient;
+const url = `mongodb+srv://${username}:${password}@heona1231.l9uh4rc.mongodb.net/?retryWrites=true&w=majority`
+let mydb;
+mongoclient.connect(url)
+    .then(client => {
+        mydb = client.db('2024WebPrograming');
+
+        app.listen(8000, function () {
+            console.log("포트 8080으로 서버 대기중...");
+        });
+    }).catch(err => {
+        console.log(err);
+    });
+
+// var mysql = require("mysql");
+// var conn = mysql.createConnection({
+//     host:"localhost",
+//     user:"root",
+//     password:"1231",
+//     database:"yourboard",
+// });
+
+//conn.connect();
 
 // conn.query("select * from todolist", function(err, rows, fields){
 //     if(err) throw err;
@@ -15,23 +32,41 @@ conn.connect();
 
 const express = require('express');
 const app = express();
+const path = require('path');
+app.use(express.static(__dirname + '/nav'));
+app.use(express.static(__dirname + '/signup'));
 
-app.listen(8080, function(){
-    console.log('포트 8080으로 서버 대기중...')
-});
+const bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({extended:true}));
 
-app.get('/book', function(req, res){
-    res.send('도서 목록 관련 페이지입니다.');
-})
-
-app.get('/', function(req, res){
+app.get('/', (req, res) => {
     res.sendFile(__dirname + '/index.html');
 })
 
-app.get('/list', function(req, res){
-    conn.query("select * from todolist", function(err, rows, fields){
-        if(err) throw err;
-        console.log(rows);
-    });    
-    console.log('데이터베이스를 조회합니다.');
+app.get('/nav.html', (req, res) => {
+    res.sendFile(path.join(__dirname, 'nav/nav.html'));
 });
+
+app.get('/signup', (req, res) => {
+    res.sendFile(__dirname + '/signup/signup.html');
+})
+
+app.post('/signup', function(req, res){
+    console.log(req);
+    console.log("저장완료");
+})
+
+// app.listen(8080, function(){
+//     console.log('포트 8080으로 서버 대기중...')
+// });
+
+app.get('/list', function (req, res) {
+    // conn.query("select * from todolist", function(err, rows, fields){
+    //     if(err) throw err;
+    //     console.log(rows);
+    // });
+    mydb.collection('UserIdPw').find().toArray().then(result => {
+        console.log(result);
+    })
+});
+
